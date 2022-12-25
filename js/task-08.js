98c40579080ac3415389f5a94c0ed240d23fd6e5
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2022-12-25 02:14:58 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -4,10 +4,12 @@ const emailEl = formEl.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
   const { email, password } = event.currentTarget.elements;
-  if (email.value === "" || password.value === "") {
+  if (email.value !== "" && password.value !== "") {
+    const userData = { email: email.value, password: password.value };
+    console.log(userData);
+  } else {
     alert("Всі поля мають бути заповнені!");
   }
-  const userData = { email: email.value, password: password.value };
-  console.log(userData);
+
   event.currentTarget.reset();
 }
